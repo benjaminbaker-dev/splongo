e759c74e0dc749d49e47cbf01584c4b844adb5a5
benjaminbaker-dev <benjaminbakercty@gmail.com>
2020-04-26 22:29:45 +0300

    added options for commands yeet

--- mongo.py
+++ mongo.py
@@ -1,1 +1,26 @@
-from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration
+from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration, Option, validators
+
+
+@Configuration()
+class Splongo(GeneratingCommand):
+    hostname = Option(
+        doc='''
+        **Syntax:** **hostname=***<hostname>*
+        **Description:** MongoDB host to connect to
+        ''',
+        require=True,
+        validate=validators.Fieldname()
+    )
+    query = Option(
+        doc='''
+        **Syntax:** **query=***<query>*
+        **Description:** query to perform, json.loads() will be called on field
+        ''',
+        require=True
+    )
+
+    def generate(self):
+        pass
+
+
+dispatch(Splongo)
